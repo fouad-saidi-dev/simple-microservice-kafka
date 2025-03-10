34fc5a277fc7f4694b0c2e91f1df6dfa5940e934
FOUAD SAIDI <fouadsai17@gmail.com>
2025-03-10 01:43:35 +0000

    Added manual acknowledgment configuration for Kafka consumer

--- notification-service/src/main/java/com/fouadev/notificationservice/listners/KafkaConsumer.java
+++ notification-service/src/main/java/com/fouadev/notificationservice/listners/KafkaConsumer.java
@@ -1,7 +1,10 @@
 package com.fouadev.notificationservice.listners;
 
 
+import com.fouadev.orderservice.event.Order;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 /*
@@ -13,9 +16,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "order-topic", groupId = "notification-group")
-    public void listen(com.fouadev.orderservice.event.Order message) {
-        System.out.println("Received message: " + message);
-        System.out.println("Order ID: " + message.getOrderId());
+//    @KafkaListener(topics = "order-topic", groupId = "notification-group")
+//    public void listen(com.fouadev.orderservice.event.Order message) {
+//        System.out.println("Received message: " + message);
+//        System.out.println("Order ID: " + message.getOrderId());
+//    }
+
+    @KafkaListener(topics = "order-topic", groupId = "notification-group",containerFactory = "kafkaListenerContainerFactory")
+    public void listen(ConsumerRecord<String,Order> record, Acknowledgment acknowledgment) {
+        try {
+            // Process the message
+            Order order = record.value();
+            System.out.println("Consumed order: " + order);
+
+            // After successful processing, commit the offset manually
+            acknowledgment.acknowledge();  // This commits the offset of the current message
+
+        } catch (Exception e) {
+            // Handle any exceptions (e.g., retry logic or DLQ)
+            System.err.println("Error consuming message: " + e.getMessage());
+        }
     }
 }
